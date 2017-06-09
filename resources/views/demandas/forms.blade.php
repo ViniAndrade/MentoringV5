295606e8a367a78511a93a295d9328b8e87cd7a5
Vinicius <vini.boy011@gmail.com>
2017-06-09 19:48:47 -0300

    botoes responsivos

---
+++ resources/views/demandas/forms.blade.php
@@ -0,0 +1,40 @@
+
+    <div class="box-body">
+
+        <div class="form-group">
+            <div class="col-sm-6">
+                {!! Form::hidden('email', null, ['class' => 'form-control', 'placeholder' => 'E-mail', 'name' => 'email']) !!}
+            </div>
+        </div>
+
+        <div class="form-group">
+            {!! Form::label('title', 'Título', ['class' => 'col-sm-2 control-label']) !!}
+            <div class="col-sm-6">
+                {!! Form::text('title', null, ['class' => 'form-control', 'placeholder' => 'Título', 'name' => 'title']) !!}
+            </div>
+        </div>
+
+        <div class="form-group">
+            {!! Form::label('subject', 'Assunto', ['class' => 'col-sm-2 control-label']) !!}
+            <div class="col-sm-6">
+                {!! Form::text('subject', null, ['class' => 'form-control', 'placeholder' => 'Assunto', 'name' => 'subject']) !!}
+            </div>
+        </div>
+
+        <div class="form-group">
+            {!! Form::label('doubt', 'Dúvida', ['class' => 'col-sm-2 control-label']) !!}
+            <div class="col-sm-6">
+                {{ Form::textarea('doubt', null, ['rows' => '10', 'cols' => '100', 'style' => 'reszie:none;']) }}
+            </div>
+        </div>
+
+			
+	<div class="form-group">
+		<label class="col-sm-2 control-label"></label>
+		<div class="col-md-2">
+				  {!! Form::submit('Enviar', ['class' => 'btn btn-block btn-success btn-flat']) !!}         
+			</div>
+    </div>
+
+
+    </div>
